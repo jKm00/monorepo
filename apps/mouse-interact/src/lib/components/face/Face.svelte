cc466b2b4598041d07af48405bd2a0c6672d142a
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-05-02 21:52:41 +0200

    feat: added mouth

--- apps/mouse-interact/src/lib/components/face/Face.svelte
+++ apps/mouse-interact/src/lib/components/face/Face.svelte
@@ -4,4 +4,13 @@
 
 <div>
 	<EyeWrapper />
+	<!-- Mouth -->
+	<div class="mouth | border-foreground aspect-square w-full rounded-full border-2"></div>
 </div>
+
+<style scoped>
+	.mouth {
+		margin-top: -5rem;
+		clip-path: polygon(0 80%, 100% 80%, 100% 100%, 0 100%);
+	}
+</style>
